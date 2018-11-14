97bb52643a70b71cec1231bd257b35e1bafb5e71
Kamran Uddin <kuddin@nyc.yearup.org>
2018-11-14 16:53:15 -0500

    pokedex

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+/*
+class pokemon(){
+  constructor(name,attack,defense,abilities){
+    this.pokeName=name;
+    this.pokeAttack=attack;
+    this.pokeDefense=defense;
+    this.pokeAbilities=abilities;
+
+  }
+}
+*/
