2a5271ab0a56d05d68026e0922c147e5c3658af2
Kamran Uddin <kuddin@nyc.yearup.org>
2018-11-15 16:50:40 -0500

    added pokeball and second background img

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
+axios.get
+  ("http://fizal.me/pokeapi/api/v2/name/ho-oh.json")
+    .then(function(response){
+
+      console.log(response);
+
+      let hp = response.data.stats[5].base_stat;
+      console.log(hp);
+      let ability = response.data.abilities[0].ability.name;
+      console.log(ability);
 
 
+});
 /*
 class pokemon(){
   constructor(name,attack,defense,abilities){
